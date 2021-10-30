a5ce11aedd1d4dd4911b3580662a532cd02a27da
ojcarlos <carlos.ojunior00@gmail.com>
2021-10-30 16:39:56 -0400

    configurações e primeiros componentes

---
+++ src/components/Titulo.tsx
@@ -0,0 +1,7 @@
+export default function Titulo(props){
+    return(
+        <div>
+            <h1>{props.children}</h1>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div className={
+    `
+    flex h-screen justify-center items-center
+    bg-gradient-to-r from-blue-500 to-purple-500
+    `
+    }>
+      <span>kjkjkj</span>
+      
+    </div>
+  )
+}
